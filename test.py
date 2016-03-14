4ec46f9c54ff1c5737d6bba666dba62557379ed2
Jorgen <jorgen@kvalvaag.org>
2016-03-15 00:21:28 +0100

    c2

--- test.py
+++ test.py
@@ -2,18 +2,15 @@ from flash import Flasher
 from intelhex import IntelHex as ih
 import time
 
-a = ih()
-a.loadhex('sd.hex')
-a.loadhex('app.hex')
-b = len(a)
-print b+1
-
-a.puts(b+1, "String here")
-a.write_hex_file('test.hex')
-
-
-f = Flasher()
-f.flash('test.hex')
-time.sleep(1)
-string = f.memrd(b+1, 11)
-print ''.join(chr(x) for x in string)
+for i in range(10):
+    a = ih()
+    a.loadhex('sd.hex')
+    a.loadhex('app.hex')
+    image_size = len(a)
+    a.puts(image_size+1, "TestImage"+str(i))
+    a.write_hex_file('test.hex')
+    f = Flasher()
+    f.flash('test.hex')
+    time.sleep(1)
+    string = f.memrd(image_size+1, 15)
+    print ''.join(chr(x) for x in string)
